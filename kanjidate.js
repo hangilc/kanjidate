fdae2d5644e59d18d0b5a5918f2e578a19779291
Hangil Chang <hangil@chang.jp>
2016-06-02 18:53:40 +0900

    extending toKanji

--- kanjidate.js
+++ kanjidate.js
@@ -101,9 +101,97 @@ function fromGengou(gengou, nen){
 exports.fromGengou = fromGengou;
 
 function toKanji(year, month, day, opt){
-	opt = opt || {};
 	var g = toGengou(year, month, day);
-	return g.gengou + g.nen + "年" + month + "月" + day + "日";
+	if( !opt ){
+		return g.gengou + g.nen + "年" + month + "月" + day + "日";
+	}
+	var format = opt.format || "GN年M月D日";
+	var parts = format.split("").map(function(ch){
+		switch(ch){
+			case "G": return formatGengou(opt.G, g.gengou);
+			case "N": return formatNen(opt.N, g.nen);
+			default: return ch;
+		}
+	});
+	return parts.join("");
 }
 
 exports.toKanji = toKanji;
+
+function gengouToAlpha(gengou){
+	switch(gengou){
+		case "平成": return "Heisei";
+		case "昭和": return "Shouwa";
+		case "大正": return "Taishou";
+		case "明治": return "Meiji";
+		default: throw new Error("unknown gengou: " + gengou);
+	}
+}
+
+function formatGengou(style, gengou){
+	style = style || "平成";
+	if( ["平成", "昭和", "大正", "明治"].indexOf(style) >= 0 ){
+		return gengou;
+	}
+	if( ["平", "昭", "大", "明"].indexOf(style) >= 0 ){
+		return gengou[0];
+	}
+	if( ["H", "S", "T", "M"].indexOf(style) >= 0 ){
+		return gengouToAlpha(gengou)[0];
+	}
+	if( ["Heisei", "Shouwa", "Taishou", "Meiji"].indexOf(style) >= 0 ){
+		return gengouToAlpha(gengou);
+	}
+	if( typeof style === "function" ){
+		return style(gengou);
+	}
+	throw new Error("unknown gengou style: " + style);
+}
+
+function padLeft(str, n, ch){
+	var m = n - str.length;
+	var pad = "";
+	while( m-- > 0 ){
+		pad += ch;
+	}
+	return pad + str;
+}
+
+var zenkakuDigits = ["０", "１", "２", "３", "４", "５", "６", "７", "８", "９"];
+var alphaDigits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+function isZenkakuDigit(ch){
+	return zenkakuDigits.indexOf(ch) >= 0;
+}
+
+function isAlphaDigit(ch){
+	return alphaDigits.indexOf(ch) >= 0;
+}
+
+function alphaDigitToZenkaku(ch){
+	var i = alphaDigits.indexOf(ch);
+	return i >= 0 ? zenkakuDigits[i] : ch;
+}
+
+function formatNen(style, nen){
+	style = style || "1";
+	var styleChars = style.split("");
+	var nenStr = nen.toString();
+	var retval;
+	if( styleChars.every(isZenkakuDigit) ){
+		retval = styleChars.map(alphaDigitToZenkaku).join("");
+		if( style[0] === "０" ){
+			return padLeft(retval, style.length, style[0]);
+		} else {
+			return retval;
+		}
+	}
+	if( styleChars.every(isAlphaDigit) ){
+		if( style[0] === "0" ){
+			return padLeft(nenStr, style.length, style[0]);
+		} else {
+			return nenStr;
+		}
+	}
+	throw new Error("unknown nen style: " + style);
+}
