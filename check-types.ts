d8f07b40d507772eedd2035243d3518cc4a73370
hangilc <hangil@chang.jp>
2016-12-31 09:07:10 +0900

    fixed kanjidate.d.ts

--- check-types.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="kanjidate.d.ts" />
-
-import * as kanjidate from "kanjidate"
-
-let s = kanjidate.format()
-let ss = kanjidate.format(new Date())
-let sss = kanjidate.format("2017-03-23")
-let t:string = kanjidate.f1
