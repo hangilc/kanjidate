17b984d27d15b663a45b42a2f8e4b3a4bca9d69a
hangilc <hangil@chang.jp>
2022-10-03 22:51:36 +0900

    cleanup

--- dist/kanjidate.d.ts
+++ dist/kanjidate.d.ts
@@ -1,33 +1,31 @@
-interface GengouNen {
-	gengou: string;
-	nen: number;
+declare class Wareki {
+    gengou: string;
+    nen: number;
+    constructor(gengou: string, nen: number);
 }
-declare function format(): string;
-declare function format(date: Date): string;
-declare function format(sqlDate: string): string;
-declare function format(fmt: string, year: number, month: number, day: number): string;
-declare function format(fmt: string, year: number, month: number, day: number,
-	hour: number, minute: number, second: number): string;
-declare function format(fmt: string, date: Date): string;
-declare function format(fmt: string, sqlDate: string): string;
-declare function toGengou(year: number, month: number, day: number): GengouNen;
+declare function toGengou(year: number, month: number, day: number): Wareki;
 declare function fromGengou(gengou: string, nen: number): number;
 declare function toYoubi(dayOfWeek: number): string;
-declare const f1: string;
-declare const f2: string;
-declare const f3: string;
-declare const f4: string;
-declare const f5: string;
-declare const f6: string;
-declare const f7: string;
-declare const f8: string;
-declare const f9: string;
-declare const f10: string;
-declare const f11: string;
-declare const f12: string;
-declare const f13: string;
-declare const f14: string;
-declare const fSqlDate: string;
-declare const fSqlDateTime: string;
+declare const f1 = "{G}{N}\u5E74{M}\u6708{D}\u65E5\uFF08{W}\uFF09";
+declare const f2 = "{G}{N}\u5E74{M}\u6708{D}\u65E5";
+declare const f3 = "{G:a}{N}.{M}.{D}";
+declare const f4 = "{G}{N:2}\u5E74{M:2}\u6708{D:2}\u65E5\uFF08{W}\uFF09";
+declare const f5 = "{G}{N:2}\u5E74{M:2}\u6708{D:2}\u65E5";
+declare const f6 = "{G:a}{N:2}.{M:2}.{D:2}";
+declare const f7 = "{G}{N}\u5E74{M}\u6708{D}\u65E5\uFF08{W}\uFF09 {a}{h:12}\u6642{m}\u5206{s}\u79D2";
+declare const f8 = "{G}{N:2}\u5E74{M:2}\u6708{D:2}\u65E5\uFF08{W}\uFF09 {a}{h:12,2}\u6642{m:2}\u5206{s:2}\u79D2";
+declare const f9 = "{G}{N}\u5E74{M}\u6708{D}\u65E5\uFF08{W}\uFF09 {a}{h:12}\u6642{m}\u5206";
+declare const f10 = "{G}{N:2}\u5E74{M:2}\u6708{D:2}\u65E5\uFF08{W}\uFF09 {a}{h:12,2}\u6642{m:2}\u5206";
+declare const f11 = "{G}{N:z}\u5E74{M:z}\u6708{D:z}\u65E5";
+declare const f12 = "{G}{N:z,2}\u5E74{M:z,2}\u6708{D:z,2}\u65E5";
+declare const f13 = "{Y}-{M:2}-{D:2}";
+declare const f14 = "{Y}-{M:2}-{D:2} {h:2}:{m:2}:{s:2}";
+declare const fSqlDate = "{Y}-{M:2}-{D:2}";
+declare const fSqlDateTime = "{Y}-{M:2}-{D:2} {h:2}:{m:2}:{s:2}";
+declare function format(): string;
+declare function format(arg: string | Date): string;
+declare function format(fmt: string, date: Date): string;
+declare function format(fmt: string, date: string): string;
+declare function format(fmt: string, year: number, month: number, day: number, hour?: number, minute?: number, second?: number): string;
 
-export { GengouNen, f1, f10, f11, f12, f13, f14, f2, f3, f4, f5, f6, f7, f8, f9, fSqlDate, fSqlDateTime, format, fromGengou, toGengou, toYoubi };
+export { f1, f10, f11, f12, f13, f14, f2, f3, f4, f5, f6, f7, f8, f9, fSqlDate, fSqlDateTime, format, fromGengou, toGengou, toYoubi };
